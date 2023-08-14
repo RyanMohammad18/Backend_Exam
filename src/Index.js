5746e6b1297065faea58ffb6f03b45468074b747
Ryan <kabirraiyan23@gmail.com>
2023-08-15 04:11:13 +0600

    msg

--- src/Index.js
+++ src/Index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { request } = require("http");
 const app = express();
-
+const mongoose = require("mongoose"); 
 const path = require("path");
 const ejs = require("ejs");
-const {collection,collection2} = require("./mongodb");
+const { collection, collection2 } = require("./mongodb");
 
+const bodyParser = require('body-parser'); //for body parser endpoint
 
 const templatepath = path.join(__dirname, "../templates");
 
@@ -14,6 +15,8 @@ app.use(express.json());
 app.set("view engine", "ejs");
 app.set("views", templatepath);
 app.use(express.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());//for endpoint
 
 app.get("/", (req, res) => {
   res.render("login");
@@ -24,12 +27,14 @@ app.get("/signup", (req, res) => {
 });
 
 app.get("/home", (req, res) => {
-  res.render("Home");
+  // Extract user_id from query parameter
+  res.render("Home"); // Pass the user_id to the template
 });
 
 //for signup database store connection
 app.post("/signup", async (req, res) => {
   const data = {
+    user_id: new mongoose.Types.ObjectId(), 
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
@@ -55,28 +60,94 @@ app.post("/login", async (req, res) => {
   }
 });
 
+//for searching
 
-//for searching 
-
-app.post('/check', async (req, res) => {
-  const input = req.body.input.split(',').map(num => parseInt(num));
+app.post("/check", async (req, res) => {
+  const input = req.body.input.split(",").map((num) => parseInt(num));
   const random = parseInt(req.body.random);
 
-  const sortedInput = input.sort((a, b) => b - a); 
+  
+
+  const sortedInput = input.sort((a, b) => b - a);
 
   const result = input.includes(random);
 
-  try {
-    await collection2.create({ input: sortedInput.join(',') }); // Store input in database
-    res.render('home', { result });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("An error occurred");
-  }
-
   
-  res.render('home', { result });
+
+    try {
+      await collection2.create({ input: sortedInput.join(",") });
+      res.render("home", { result: result });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("An error occurred");
+    }
+ 
 });
+
+
+
+//for endpoint
+
+const inputData = [
+  {
+    timestamp: '2012-01-01 00:00:00',
+    user_id: 1,
+    input_values: '11, 10, 9, 7, 5, 1, 0'
+  },
+  {
+    timestamp: '2013-01-01 01:00:00',
+    user_id: 1,
+    input_values: '13, 11, 10, 7, 5, 2, 1'
+  },
+  // ... other data entries
+];
+
+app.get('/get-input-values', (req, res) => {
+  const { start_datetime, end_datetime, user_id } = req.query;
+  
+  // Validate user_id, start_datetime, and end_datetime here
+
+  const filteredData = inputData.filter(entry => {
+    return (
+      entry.user_id == user_id &&
+      entry.timestamp >= start_datetime &&
+      entry.timestamp <= end_datetime
+    );
+  });
+
+  const response = {
+    status: 'success',
+    user_id: user_id,
+    payload: filteredData
+  };
+
+  res.json(response);
+});
+
+
+app.post('/add-input-values/:user_id', (req, res) => {
+  const { user_id } = req.params;
+  const { input_values } = req.body;
+
+  const timestamp = new Date().toISOString();
+  
+  const entry = {
+    timestamp: timestamp,
+    user_id: parseInt(user_id),
+    input_values: input_values.join(', ')
+  };
+
+  inputData.push(entry);
+
+  const response = {
+    status: 'success',
+    message: 'Input values added successfully'
+  };
+
+  res.json(response);
+});
+
+
 
 
 app.listen(5000, () => {
--- src/mongodb.js
+++ src/mongodb.js
@@ -1,43 +1,46 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/LoginSignup")
-.then(() => {
-    console.log('Connected to MongoDB')
-})
-.catch(()=>{
-    console.log("failed to connect")
-}) 
+mongoose
+  .connect("mongodb://127.0.0.1:27017/LoginSignup")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch(() => {
+    console.log("failed to connect");
+  });
 
 const LoginSchema = new mongoose.Schema({
-    name:{
-        type : String,
-        required:[true,"Please enter your Name"]
-    },
-    email:{
-        type: String,
-        required:[true,"Please enter your Name"]
-    },
-    password:{
-        type: String,
-        required:[true,"Please enter your password"]
-    }
-})
+  
+  name: {
+    type: String,
+    required: [true, "Please enter your Name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter your Name"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter your password"],
+  },
+});
 
 const Inputschema = new mongoose.Schema({
+ 
+  input: {
+    type: String,
+    required: [true, "Please enter your values"],
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-    input:{
-        type : String,
-        required:[true,"Please enter your values"]
-
-    }
-
-})
-
-const collection = new mongoose.model("Collection-1",LoginSchema)
-const collection2 = new mongoose.model("Collection-2",Inputschema)
-
+const collection = new mongoose.model("Collection-1", LoginSchema);
+const collection2 = new mongoose.model("Collection-2", Inputschema);
 
 module.exports = {
-    collection,
-    collection2
+  collection,
+  collection2,
 };
